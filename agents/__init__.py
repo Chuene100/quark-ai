95c43205667842793331f539931f69d3d8934f28
Chuene-raphta <chuene@raphta.com>
2026-04-10 16:03:55 +0200

    feat: Changed the init file in agents

---
+++ agents/__init__.py
@@ -0,0 +1,5 @@
+from agents.base_agent import BaseAgent, Message
+from agents.cover_letter_agent import CoverLetterAgent
+from agents.networking_agent import NetworkAgent
+from agents. cv_review_agent import CVReviewAgent
+from agents.interview_prep_agent import InterviewPrepAgent
